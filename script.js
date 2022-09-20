b48a0fd96011cd8cef1e67924cc0f11bd23da582
gabriel <gabriel.psilva226@gmail.com>
2022-09-20 00:59:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function setInvisible() {
+    document.getElementById("nothingToShow").classList.add("invisible");
+}
+
+function setVisible() {
+    document.getElementById("nothingToShow").classList.remove("invisible");
+}
